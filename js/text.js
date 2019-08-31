8c44699f7d4493fb09a7d841438038e7fee096b7
Oksana Kosyieva <oksankak16@gmail.com>
2019-08-31 15:23:58 +0000

    js

---
+++ js/text.js
@@ -0,0 +1,19 @@
+let message = 'Hello JavaScript';
+alert(message);
+message = "Nice to see you";
+alert(message);
+let youserAge=parseInt(prompt('Enter your age'));
+// alert('your age is ' + youserAge);
+let nextAge=addOne(youserAge);
+alert('next year your age is ' + nextAge);
+if (youserAge>=18){
+    alert("You can drink");
+}
+else {
+alert("you cant drink";)
+};
+
+function addOne (value){
+    const newValue=value+1;
+    return newValue;
+}
